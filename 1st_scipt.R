ed8958e3ad83e507709aeaaa636f4594ae6be7aa
Ko Ko Zaw <124769040+zawkzaw2@users.noreply.github.com>
2023-02-24 12:48:28 +0630

    Add files via upload

---
+++ 1st_scipt.R
@@ -0,0 +1,15 @@
+library(googlesheets4)
+lbw <- read_sheet("https://docs.google.com/spreadsheets/d/1mCuEkfUo-1L4_g__D2SBsIyqYTNdi1spIFpDSeUFiag/edit?usp=sharing")
+
+str(lbw)
+summary(lbw)
+dim(lbw)
+nrow(lbw)
+ncol(lbw)
+table(lbw$race)
+table(lbw$low)
+
+hist(lbw$bwt)
+
+
+
